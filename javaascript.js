56f64ee3f93851b80cf3672d32656d1b3dc5725e
ariyamn <ariyamoqadam@gmail.com>
2022-07-07 14:45:15 +0430

    I shit this project! ha ha ha ~...

--- javaascript.js
+++ javaascript.js
@@ -81,27 +81,7 @@ class Car {
     this.name = name;
     this.year = year;
   }
-  age(x){
-    return x - this.year;
-  }
-}
-let date = new Date();
-let year = date.getFullYear();
 
-let myCar =new Car("Ford",2005);
-document.getElementById("demo-2").innerHTML=
-  "My car is" + myCar.age(year) + "years old";
-
-// $(function() {
-//   $("button").on({
-//     mouseover: function() {
-//       $(this).css({
-//         left: (Math.random() * 100) + "%",
-//         top: (Math.random() * 100) + "%",
-//       });
-//     }
-//   });
-// });
 
 
 const points = [40, 100, 1, 5, 25, 10];
